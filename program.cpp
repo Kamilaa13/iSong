695acbf8f8ab49e8b7e0ff15940e417be9d3ff48
Kamilaa13 <kamilaputrihasan@gmail.com>
2024-10-05 13:27:28 +0700

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void tampilkanGenre()
+void tampilkanGenre() {
+cout << "Daftar Genre Lagu : " << endl;
+cout << "1. Pop" << endl;
+cout << "2. Rock" << endl;
+cout << "3. Jazz" << endl;
+cout << "4. Klasik" << endl;
+cout << "5. Kpop" << endl;
+}
+
+
