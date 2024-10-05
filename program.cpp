83ce7845ff5af15d966a66a7a5fc7aeca05c2e5e
sindy-meylindasari <sindyms446@gmail.com>
2024-10-05 13:31:34 +0700

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -10,4 +10,27 @@ cout << "4. Klasik" << endl;
 cout << "5. Kpop" << endl;
 }
 
+void pilihanGenre(int pilihan) {
+    switch(pilihan) {
+        case 1:
+            cout << "Genre Pop: "<<endl;
+            break;
+        case 2:
+            cout << "Genre Rock: "<<endl;
+            break;
+        case 3:
+            cout << "Genre Jazz: "<<endl;
+            break;
+        case 4:
+            cout << "Genre Klasik: "<<endl;
+            break;
+        case 5:
+            cout << "Genre Kpop: "<<endl;
+            break;
+        default:
+            cout << "Pilihan tidak valid"<<endl;
+            break;
+    }
+}
+
 
